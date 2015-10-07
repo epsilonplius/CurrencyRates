de5d6610736afceb78cd5299b808cdbf19d0a94e
epsilonplius <aleksandras@epsilonplius.com>
2015-10-07 22:40:49 +0300

    Lb xml deserialize test

--- CurrencyRates.Tests/Controllers/HomeControllerTest.cs
+++ CurrencyRates.Tests/Controllers/HomeControllerTest.cs
@@ -50,5 +50,6 @@ namespace CurrencyRates.Tests.Controllers
             // Assert
             Assert.IsNotNull(result);
         }
+
     }
 }
---
+++ CurrencyRates.Tests/HelperTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using CurrencyRates.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace CurrencyRates.Tests
+{
+    [TestClass]
+    public class HelperTests
+    {
+        [TestMethod]
+        public void LbDeserializeTest()
+        {
+            DateTime? currDate = new DateTime(2014, 1, 1);
+
+            var lbClient = new RestClient(ConfigurationManager.AppSettings["lbBaseUri"]);
+            var req = new RestRequest(String.Format(ConfigurationManager.AppSettings["lbCurrencyServiceUrl"], currDate.Value.ToShortDateString()), Method.GET);
+            var resp = lbClient.Get(req);
+
+            //====================
+
+            //var rates = new ExchangeRates();
+            var serializer = new XmlSerializer(typeof(ExchangeRates));
+
+            var reader = XmlReader.Create(new StringReader(resp.Content));
+
+            var rates = (ExchangeRates)serializer.Deserialize(reader);
+
+            //====================
+
+            Assert.IsNotNull(rates);
+            Assert.IsNotNull(rates.item);
+            Assert.IsTrue(rates.item.Any());
+            
+
+        }
+    }
+}
